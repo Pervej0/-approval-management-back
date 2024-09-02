7ba44980dc2cdc6f31e0be1ccfc8352c7f2755dd
Pervej0 <mdparvez222khan@gmail.com>
2024-09-02 16:29:57 +0600

    query builder added

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -20,9 +20,32 @@ class QueryBuilder<T> {
   }
   filter() {
     const queryObj: Record<string, unknown> = { ...this.query };
-    const excludeFields = ["searchTerm"];
+    const excludeFields = ["searchTerm", "sort"];
     excludeFields.map((item) => delete queryObj[item]);
     this.modelQuery = this.modelQuery.find(queryObj);
+    return this;
+  }
+  sort() {
+    const sortable = (this.query?.sort as string) || "-createdAt";
+    this.modelQuery = this.modelQuery.sort(sortable);
+    return this;
+  }
+  limit() {
+    const limitable = Number(this.query.limit || 10);
+    this.modelQuery = this.modelQuery.limit(limitable);
+    return this;
+  }
+  paginate() {
+    const limit = Number(this.query.limit || 10);
+    const page = Number(this.query.page || 1);
+    const skip = (page - 1) * limit;
+    this.modelQuery = this.modelQuery.skip(skip).limit(limit);
+    return this;
+  }
+  fields() {
+    let fieldsData = "-_v";
+    fieldsData = this.query.fields as string;
+    this.modelQuery = this.modelQuery.select(fieldsData);
     return this;
   }
 }
